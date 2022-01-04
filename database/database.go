7805f907a5576201d84c3fb9ac0daa938672f290
Mager <andrew.mager@gmail.com>
2022-01-03 20:30:44 -0600

    Make photo bool

--- database/database.go
+++ database/database.go
@@ -49,7 +49,7 @@ type User struct {
 	ENSName     string   `firestore:"ensName" json:"ensName"`
 	Slug        string   `firestore:"slug" json:"slug"`
 	Name        string   `firestore:"name" json:"name"`
-	Photo       string   `firestore:"photo" json:"photo"`
+	Photo       bool     `firestore:"photo" json:"photo"`
 	Twitter     string   `firestore:"twitter" json:"twitter"`
 	OpenSea     string   `firestore:"openSea" json:"openSea"`
 	IsWhale     bool     `firestore:"isWhale" json:"isWhale"`
